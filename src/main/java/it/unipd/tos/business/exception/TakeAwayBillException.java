e5ab1c49528a7e34a15f64cc4211303f77cd3a3b
Daniel Eduardo Contro <dani.contro2@gmail.com>
2020-12-09 00:53:08 +0100

    Creazione della classe TakeAwayBillException

---
+++ src/main/java/it/unipd/tos/business/exception/TakeAwayBillException.java
@@ -0,0 +1,12 @@
+////////////////////////////////////////////////////////////////////
+// Daniel Eduardo Contro 1187597
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.tos.business.exception;
+
+public class TakeAwayBillException extends Exception {
+
+    public TakeAwayBillException(String message) {
+        super(message);
+    }
+}
